97167360c4698a8fd89f5b33a27e85aeb90256c2
sheikhussama <sheikhussama1994@gmail.com>
2020-06-09 11:22:29 -0700

    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
 
 const path = require('path');
 
